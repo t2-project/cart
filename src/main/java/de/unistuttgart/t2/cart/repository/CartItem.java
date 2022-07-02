d4431d531602fbf7af8291a2b3ec2a5495b989ad
delvh <leon@kske.dev>
2022-07-02 14:28:58 +0200

    Format all classes in cart with my formatter

--- src/main/java/de/unistuttgart/t2/cart/repository/CartItem.java
+++ src/main/java/de/unistuttgart/t2/cart/repository/CartItem.java
@@ -1,67 +1,64 @@
 package de.unistuttgart.t2.cart.repository;
 
 import java.time.Instant;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.data.annotation.Id;
 
 /**
- * 
- * The content of someones cart. 
- * 
+ * The content of someones cart.
  * <p>
- * Cart items have a {@code creationDate} such that they might be killed after
- * they exceeded their time to life. 
- * 
- * @author maumau
+ * Cart items have a {@code creationDate} such that they might be killed after they exceeded their time to life.
  *
+ * @author maumau
  */
 public class CartItem {
-	@Id
-	private String id;
-	private Map<String, Integer> content;
-	
-	private Date creationDate; 
-	
-	public CartItem(String id, Map<String, Integer> content, Date creationDate) {
-		super();
-		this.id = id;
-		this.content = content;
-		this.creationDate = creationDate;
-	}
-	
-	public CartItem(String id, Map<String, Integer> content) {
-		this(id, content, Date.from(Instant.now()));
-	}
-	
-	public CartItem(String id) {
-		this(id, new HashMap<>(), Date.from(Instant.now()));
-	}
-	
-	public CartItem() {
-		this(null, new HashMap<>(), Date.from(Instant.now()));
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public Map<String, Integer> getContent() {
-		return content;
-	}
-	public void setContent(Map<String, Integer> content) {
-		this.content = content;
-	}
 
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    @Id
+    private String id;
+    private Map<String, Integer> content;
 
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
+    private Date creationDate;
+
+    public CartItem(String id, Map<String, Integer> content, Date creationDate) {
+        this.id = id;
+        this.content = content;
+        this.creationDate = creationDate;
+    }
+
+    public CartItem(String id, Map<String, Integer> content) {
+        this(id, content, Date.from(Instant.now()));
+    }
+
+    public CartItem(String id) {
+        this(id, new HashMap<>(), Date.from(Instant.now()));
+    }
+
+    public CartItem() {
+        this(null, new HashMap<>(), Date.from(Instant.now()));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, Integer> getContent() {
+        return content;
+    }
+
+    public void setContent(Map<String, Integer> content) {
+        this.content = content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
--- src/test/java/de/unistuttgart/t2/cart/TestContext.java
+++ src/test/java/de/unistuttgart/t2/cart/TestContext.java
@@ -1,8 +1,7 @@
 package de.unistuttgart.t2.cart;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import de.unistuttgart.t2.cart.repository.CartRepository;
@@ -10,8 +9,7 @@ import io.opentracing.contrib.java.spring.jaeger.starter.JaegerAutoConfiguration
 import io.opentracing.contrib.spring.web.starter.ServerTracingAutoConfiguration;
 
 @Configuration
-@EnableAutoConfiguration(exclude = {ServerTracingAutoConfiguration.class, JaegerAutoConfiguration.class})
-@EnableMongoRepositories(basePackageClasses = {CartRepository.class})
+@EnableAutoConfiguration(exclude = { ServerTracingAutoConfiguration.class, JaegerAutoConfiguration.class })
+@EnableMongoRepositories(basePackageClasses = { CartRepository.class })
 @Profile("test")
-public class TestContext {
-}
+public class TestContext {}
